093e2ae31da252cb8d1454b3c330c8e362903f95
Joel <joellarramendia@gmail.com>
2022-07-20 21:29:15 -0400

    .

---
+++ envio_de_variables_php/index.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+<body>
+    <div style="width:40%; margin-left:auto; margin-right:auto; margin-top:100px">
+    <form action="recibe.php" method="post">
+        <h2>FICHA DE CONTACTO</h2>
+        <input type="text" class="form-control" name="nombre">
+        <input type="text" class="form-control" name="apellido">
+        <input type="text" class="form-control" name="edad">
+
+        <button type="sumbit">Enviar</button>
+    </form>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ envio_de_variables_php/recibe.php
@@ -0,0 +1,7 @@
+<?php
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$edad = $_POST['edad'];
+
+echo"hola, mi nombre es $nombre $apellido y tengo $edad";
+?>
